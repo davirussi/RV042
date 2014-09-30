2222063713f9bf7ccb12bce6518f1053c94ad74f
davi russi <davi.russi@smdh.org>
2014-09-30 09:35:23 -0300

    change dictionary of vectors to vector of dictionarys & using template from file

--- main.py
+++ main.py
@@ -1,24 +1,31 @@
 ### simple way to scrap list os ips and macs from RV042 you will need the html with the information.
 import re
+from jinja2 import Template
+from jinja2 import Environment, FileSystemLoader
 
 def scrapper(file):
-    out={}
-    ip=[]
-    mac=[]
-    name=[]
+    out=[]
     for line in file:
         if re.search("\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3} =>", line):
-            ip.append(re.search("\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}",line).group(0))
-            mac.append(re.search("([0-9a-fA-F]{2}[:-]){5}([0-9a-fA-F]{2})",line).group(0))
-            name.append(re.search("1 .{1,15}'>",line).group(0)[2:-2])
-    out['ip']=ip
-    out['mac']=mac
-    out['name']=name
+            dic={}
+            dic['ip']=(re.search("\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}",line).group(0))
+            dic['mac']=(re.search("([0-9a-fA-F]{2}[:-]){5}([0-9a-fA-F]{2})",line).group(0))
+            dic['name']=(re.search(" 1 .{1,15}'>",line).group(0)[2:-2])
+            out.append(dic)
     return out 
+
+
+def generater(dic):
+    outfile=open('out.txt','w')
+    env = Environment(loader=FileSystemLoader('.'))
+    template = env.get_template('template.txt')    
+    with open('ipmacname.txt', 'w') as f:
+        f.write(template.render(data=dic))
     
 def fun(filename):
     file=open(filename,'r')
-    list=scrapper(file)
+    dic=scrapper(file)
+    generater(dic)
 
 def main():
     fun('dhcp_setup.htm')
